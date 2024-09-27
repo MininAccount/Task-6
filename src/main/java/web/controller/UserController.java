912569be05392a5cf62187ab735dcc3cba4edece
WerpNSK <mininnick@yandex.ru>
2024-09-27 15:54:05 +0700

    (dev) Develop Task-2.3.1

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,53 @@
+package web.controller;
+
+import web.model.User;
+import web.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/")
+public class UserController {
+
+	@Autowired
+	private UserService userService;
+
+	@GetMapping("/")
+	public String ShowAllUsers(Model model) {
+		model.addAttribute("users", userService.getAllUsers());
+		return "index";
+	}
+
+	@GetMapping("/add")
+	private String ShowAddUserFrom(Model model) {
+		model.addAttribute("user", new User());
+		return "add";
+	}
+
+	@PostMapping("/add")
+	public String AddUser(@ModelAttribute("user") User user) {
+		userService.saveUser(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/edit/{id}")
+	public String ShowEditUserForm(@PathVariable("id") int id, Model model) {
+		User user = userService.getUserById(id);
+		model.addAttribute("user", user);
+		return "edit";
+	}
+
+	@PostMapping("/edit")
+	public String EditUser(@ModelAttribute("user") User user) {
+		userService.updateUser(user);
+		return "redirect:/";
+	}
+
+	@GetMapping("/delete/{id}")
+	public String DeleteUser(@PathVariable("id") int id) {
+		userService.deleteUser(id);
+		return "redirect:/";
+	}
+}
